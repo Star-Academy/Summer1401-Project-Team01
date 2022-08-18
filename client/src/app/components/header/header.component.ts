7e42df264612f77fe3244092a0ed030e3a9263e0
msrazavi <razavi.mst@gmail.com>
2022-08-18 15:04:34 +0430

    [FIX] header styles

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,10 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent {
-    public currentEnvTitle: string = '';
+    constructor() {}
+    public currentEnvTitle: string = 'انتخاب محیط کار';
 }
