1982c5fc76506601bdab05cb2e8806c830041585
msrazavi <razavi.mst@gmail.com>
2022-08-18 13:33:10 +0430

    [FIX] header styles

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -6,5 +6,5 @@ import {Component} from '@angular/core';
     styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-    public currentEnvTitle: string = 'داده‌آرا';
+    public currentEnvTitle: string = '';
 }
