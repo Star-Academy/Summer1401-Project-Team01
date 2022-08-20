5afc63fd17d77d79ee66c17f30e8d8897eb6ea59
msrazavi <razavi.mst@gmail.com>
2022-08-20 21:26:50 +0430

    [FIX] header select function

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, OnChanges, OnInit, ViewEncapsulation} from '@angular/core';
 import {Router} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
     selector: 'app-header',
@@ -8,16 +9,25 @@ import {Router} from '@angular/router';
     encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent {
-    public constructor(private router: Router) {}
-    public currentEnvTitle: string = 'انتخاب محیط کار';
+    public constructor(private router: Router, private location: Location) {
+        let path = this.location.path();
+        if (path.startsWith('/pipeline-designer')) this.currentEnvTitle = this.PIPELINE_DESIGNER;
+        else if (path.startsWith('/data-inventory')) this.currentEnvTitle = this.DATA_INVENTORY;
+    }
+
+    private SELECT_ENV = 'انتخاب محیط کار';
+    private DATA_INVENTORY = 'فهرست دادگان‌ها';
+    private PIPELINE_DESIGNER = 'مدیریت پایپلاین‌ها';
+
+    public currentEnvTitle: string = this.SELECT_ENV;
 
     public async selectDataInventory(): Promise<void> {
-        this.currentEnvTitle = 'فهرست دادگان‌ها';
+        this.currentEnvTitle = this.DATA_INVENTORY;
         await this.router.navigateByUrl('/');
     }
 
     public async selectPipelineDesigner(): Promise<void> {
-        this.currentEnvTitle = 'مدیریت پایپلاین‌ها';
+        this.currentEnvTitle = this.PIPELINE_DESIGNER;
         await this.router.navigateByUrl('pipeline-designer');
     }
 }
