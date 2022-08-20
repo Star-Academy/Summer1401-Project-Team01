b984aa479e1792105dcc80a07e309126a4f334ac
msrazavi <razavi.mst@gmail.com>
2022-08-20 18:05:45 +0430

    [ADD] pipeline designer page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {HomeComponent} from './pages/home/home.component';
+import {PipelineDesignerComponent} from './pages/pipeline-designer/pipeline-designer.component';
 
 const routes: Routes = [
     {path: '', pathMatch: 'full', component: HomeComponent},
+    {path: 'pipeline-designer', component: PipelineDesignerComponent},
     {path: '**', redirectTo: ''},
 ];
 
