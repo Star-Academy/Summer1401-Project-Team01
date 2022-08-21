51c7889656186569c53074eaa013122408f527b2
Arya Jalali <nothatcreative5@gmail.com>
2022-08-21 21:49:35 +0430

    FileController.cs

--- server/TalStart/Controllers/FileController.cs
+++ server/TalStart/Controllers/FileController.cs
@@ -6,5 +6,21 @@ namespace TalStart.Controllers;
 [Route("[controller]")]
 public class FileController
 {
-        
+    [HttpPost("/file")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> UploadFile(IFormFile file)
+    {
+        await Task.Delay(3);
+        return new BadRequestResult();
+    }
+
+    [HttpGet("/file")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> DownloadFile([FromQuery] string fileName)
+    {
+        await Task.Delay(3);
+        return new BadRequestResult();
+    }
+    
+    
 }
