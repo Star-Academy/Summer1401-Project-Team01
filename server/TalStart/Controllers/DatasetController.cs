7b28d6583d8732be5c710d54356974367bac1cdc
Arya Jalali <nothatcreative5@gmail.com>
2022-08-21 21:27:21 +0430

    DatasetController.cs

--- server/TalStart/Controllers/DatasetController.cs
+++ server/TalStart/Controllers/DatasetController.cs
@@ -16,6 +16,41 @@ public class DatasetController
         await Task.Delay(3);
         return new BadRequestResult();
     }
-    
-    
+
+    [HttpDelete("/dataset")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> RemoveDataset()
+    {
+        await Task.Delay(3);
+        return new BadRequestResult();
+    }
+
+    [HttpPatch("/dataset")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> ChangeDatasetName([FromBody] string name)
+    {
+        await Task.Delay(3);
+        return new BadRequestResult();
+    }
+
+    [HttpGet("/dataset/all")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetAllDatasets()
+    {
+        await Task.Delay(3);
+        return new BadRequestResult();
+    }
+
+    [HttpGet("/dataset/{count}")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetDatasetSample(int count)
+    {
+        await Task.Delay(3);
+        return new BadRequestResult();
+    }
+
+
+
+
+
 }
