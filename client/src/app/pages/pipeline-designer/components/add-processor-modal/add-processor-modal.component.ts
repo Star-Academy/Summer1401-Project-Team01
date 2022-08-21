067cd93f0fcc9adab2b3442cbe5be17ca2bede48
msrazavi <razavi.mst@gmail.com>
2022-08-21 12:29:18 +0430

    [ADD] add-processor-modal

---
+++ client/src/app/pages/pipeline-designer/components/add-processor-modal/add-processor-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-processor-modal',
+  templateUrl: './add-processor-modal.component.html',
+  styleUrls: ['./add-processor-modal.component.scss']
+})
+export class AddProcessorModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/src/app/pages/pipeline-designer/components/add-processor-modal/add-processor-modal.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddProcessorModalComponent } from './add-processor-modal.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddProcessorModalComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AddProcessorModalModule { }
