a114e639cb940720d8fdbe1ac0996372b9aa2543
msrazavi <razavi.mst@gmail.com>
2022-08-21 09:46:44 +0430

    [ADD] pipeline-designer template with RESIZE :)

--- client/src/app/pages/pipeline-designer/pipeline-designer.component.ts
+++ client/src/app/pages/pipeline-designer/pipeline-designer.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
     selector: 'app-pipeline-designer',
@@ -6,5 +6,48 @@ import {Component} from '@angular/core';
     styleUrls: ['./pipeline-designer.component.scss'],
 })
 export class PipelineDesignerComponent {
-    public constructor() {}
+    private isVResizing = false;
+    private isHResizing = false;
+    private lastDownX = 0;
+    private lastDownY = 0;
+    private vDragger = document.querySelector('.drag-v');
+    private hDragger = document.querySelector('.drag-h');
+
+    @ViewChild('canvas', {static: false}) public canvas!: ElementRef | undefined;
+    @ViewChild('sample', {static: false}) public sample!: ElementRef | undefined;
+    @ViewChild('side', {static: false}) public side!: ElementRef | undefined;
+    @ViewChild('designer', {static: false}) public main!: ElementRef | undefined;
+
+    public constructor(private renderer: Renderer2) {}
+
+    public dragVHandler(e: MouseEvent): void {
+        this.isVResizing = true;
+    }
+
+    public dragHHandler(e: MouseEvent): void {
+        this.isHResizing = true;
+    }
+
+    public dragGeneralHandler(e: MouseEvent): void {
+        const main = this.main!.nativeElement;
+        const canvas = this.canvas!.nativeElement;
+        const side = this.side!.nativeElement;
+        const sample = this.sample!.nativeElement;
+        if (this.isHResizing) {
+            const offsetRight = parseInt(getComputedStyle(main).width) - (e.clientX - parseInt(main.offsetLeft));
+            this.renderer.setStyle(side, 'width', offsetRight.toString() + 'px');
+            this.renderer.setStyle(canvas, 'right', offsetRight.toString() + 'px');
+        }
+        if (this.isVResizing) {
+            const offsetBottom = parseInt(getComputedStyle(main).height) - (e.clientY - parseInt(main.offsetTop));
+
+            this.renderer.setStyle(sample, 'height', offsetBottom.toString() + 'px');
+            this.renderer.setStyle(canvas, 'bottom', offsetBottom.toString() + 'px');
+        }
+    }
+
+    public freeDragHandler(): void {
+        this.isVResizing = false;
+        this.isHResizing = false;
+    }
 }
