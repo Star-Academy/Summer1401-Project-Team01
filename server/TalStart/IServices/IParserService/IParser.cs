f27649289f1290906075f65f5c60d298dc0ac41a
Arya Jalali <nothatcreative5@gmail.com>
2022-08-23 17:30:55 +0430

    Download and Upload

--- server/TalStart/IServices/IParserService/IParser.cs
+++ server/TalStart/IServices/IParserService/IParser.cs
@@ -1,6 +1,7 @@
 ﻿namespace TalStart.IServices.IParserService;
 
-public interface IParserCsvToPostgresTable
+public interface IParser
 {
+    public void ParsePostgresTableToCsv(string tableName, string filePath);
     public void ParseCsvToPostgresTable(Dictionary<string, string> columnType, string tableName, string filePath);
 }
