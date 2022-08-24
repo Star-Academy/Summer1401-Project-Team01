e9836477c85a38b7312f544c858aef11aea48da7
DnyaNvB <dnya.nvb@gmail.com>
2022-08-24 17:11:16 +0430

    DatasetService -> RemoveDataset

--- server/TalStart/Services/DatasetService.cs
+++ server/TalStart/Services/DatasetService.cs
@@ -1,26 +1,49 @@
 using TalStart.IServices;
 using TalStart.Models;
 
-namespace TalStart.Services;
-
-public class DatasetService : IDatasetService
+namespace TalStart.Services
 {
-    TalStartContext db = new();
-    public PipelineService()
+    public class DatasetService : IDatasetService
     {
+        TalStartContext db = new();
+        public DatasetService()
+        {
 
-    }
-    public bool AddDataset(string username, string datasetName)
-    {
-        try
-        {
-            db.dataSets.Add(new DataSet() { User = db.Users.SingleOrDefault(user => user.Username == username), Name = datasetName });
-            db.SaveChanges();
-            return true;
         }
-        catch (Exception e)
+        public bool AddDataset(string username, string datasetName)
         {
-            return false;
+            try
+            {
+                db.dataSets.Add(new DataSet() { User = db.Users.SingleOrDefault(user => user.Username == username), Name = datasetName });
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveDataset(string username, string datasetName)
+        {
+            try
+            {
+                var dataset = db.DataSets.FirstOrDefault(ds.User.Username == username && ds => ds.Name = datasetName);
+                if (dataset == null)
+                    return false;
+                db.DataSets.Remove(dataset);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool RenameDataset(string username, string datasetName)
+        {
+
         }
     }
 }
