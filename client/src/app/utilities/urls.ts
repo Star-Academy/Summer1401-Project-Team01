38261c986895df070d428e4a98d74c1d2f377e8a
SinaMAmini <smobasheramini@gmail.com>
2022-08-26 20:16:10 +0430

    [ADD] pipeline service

--- client/src/app/utilities/urls.ts
+++ client/src/app/utilities/urls.ts
@@ -1,5 +1,7 @@
 export const BASE_URL = 'https://localhost:7210';
 
+export const PIPLINE_GETALLPIPELINENAMES = BASE_URL + '/Pipeline/GetAllPipelinesNames/';
+
 export const DEFAULT_POST_REQUEST_INIT: RequestInit = {
     method: 'post',
     headers: {'Content-Type': 'application/json'},
