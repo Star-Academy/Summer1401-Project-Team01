09a1b565e6bd2a9236a1079108e3578a08b75705
Arya Jalali <nothatcreative5@gmail.com>
2022-08-27 14:20:10 +0430

    Merged fileController with datasetController

--- server/TalStart/Controllers/FileController.cs
+++ server/TalStart/Controllers/FileController.cs
@@ -15,32 +15,15 @@ public class FileController : ControllerBase
         _fileService = fileService;
     }
 
-    [HttpPost]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<IActionResult> UploadFile([FromHeader] IFormFile file, Dictionary<string, string> columns,
-        string username)
-
-    {
-        try
-        {
-            await _fileService.UploadFile(file, columns, username);
-            return new OkResult();
-        }
-        catch (Exception e)
-        {
-            return new BadRequestResult();
-        }
-    }
-
     [HttpPost("download")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> DownloadFile(string fileName, string username)
+    public async Task<IActionResult> DownloadFile(string datasetName, string username)
     {
         try
         {
-            return await _fileService.DownloadFile(fileName, username);
+            return await _fileService.DownloadFile(datasetName, username);
         }
         catch (Exception e)
         {
