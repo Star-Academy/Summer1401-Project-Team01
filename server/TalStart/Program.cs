0babff25cfc63af0da876f53b719fc9d2fdc0b0b
mahdi <acilic79@gmail.com>
2022-08-28 15:12:21 +0430

    injection corrected

--- server/TalStart/Program.cs
+++ server/TalStart/Program.cs
@@ -1,5 +1,6 @@
 using TalStart.IServices;
 using TalStart.IServices.IParserService;
+using TalStart.Models.ProcessType;
 using TalStart.Services;
 using TalStart.Services.ParserService;
 
@@ -11,14 +12,17 @@ var allowCors = "allowCores";
 builder.Services.AddTransient<IPipelineService, PipelineService>();
 builder.Services.AddTransient<IDatasetService, DatasetService>();
 builder.Services.AddTransient<IUserService, UserService>();
-//builder.Services.AddTransient<ISqlService, SqlService>();
+builder.Services.AddTransient<ISqlService, SqlService>();
 builder.Services.AddTransient<IQueryBuilder, QueryBuilder>();
 builder.Services.AddTransient<IScenarioService, ScenarioService>();
-builder.Services.AddTransient<IParser>(x => new Parser(x.GetRequiredService<IQueryBuilder>()));
+builder.Services.AddTransient<IParser>(x => new Parser(x.GetRequiredService<IQueryBuilder>()
+    ,x.GetRequiredService<ISqlService>()));
 builder.Services.AddTransient<IFileService>(x => new FileService(x.GetRequiredService<IParser>()));
 builder.Services.AddTransient<IDatasetService>(x => new DatasetService(x.GetRequiredService<IParser>(),
     x.GetRequiredService<ISqlService>(), x.GetRequiredService<IQueryBuilder>(), 
     x.GetRequiredService<IFileService>()));
+//builder.Services.AddScoped<FooProcess>(x => new FooProcess(x.GetRequiredService<SqlService>()));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
