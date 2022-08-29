f6284b2cd6b4b0b42fb95af1b531b9001cc75702
SinaMAmini <smobasheramini@gmail.com>
2022-08-29 12:59:36 +0430

    [ADD] fetch run processes

--- client/src/app/utilities/urls.ts
+++ client/src/app/utilities/urls.ts
@@ -8,6 +8,7 @@ export const DATASET_REMOVE = BASE_URL + '/Dataset/RemoveDataset';
 
 export const PIPELINE_GET_ALL_PIPELINE_NAMES = BASE_URL + '/Pipeline/GetAllPipelinesNames/';
 export const PIPELINE_UPDATE_PROCESSES = BASE_URL + '/Pipeline/UpdateProcesses/';
+export const PIPELINE_RUNPIPELINE = BASE_URL + '/Pipeline/RunPipeline/';
 
 export const DEFAULT_POST_REQUEST_INIT: RequestInit = {
     method: 'post',
