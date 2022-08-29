67905eebf6fdf1c0709b23ad9e0f8b98d52e2c31
Arya Jalali <nothatcreative5@gmail.com>
2022-08-30 03:14:31 +0430

    flterParam removed.

--- server/TalStart/Models/ProcessType/Options/FilterOptions.cs
+++ server/TalStart/Models/ProcessType/Options/FilterOptions.cs
@@ -4,23 +4,6 @@ namespace TalStart.Models.ProcessType.Options;
 
 public class FilterOptions
 {
-    public List<FilterParam> parameters { get; set; }
-    public bool AND { get; set; }
-
-    public override string ToString()
-    {
-        var query = new StringBuilder();
-        foreach (var param in parameters)
-        {
-            query.Append(param.ToString() + (AND ? "AND" : "OR") + "\n");
-        }
-
-        return query.ToString();
-    }
-}
-
-public class FilterParam
-{
     public string columnName { get; set; }
     public string @operator { get; set; }
     public string value { get; set; }
