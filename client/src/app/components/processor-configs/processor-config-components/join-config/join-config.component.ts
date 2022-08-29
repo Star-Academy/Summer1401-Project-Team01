2f1f59e20593d7059c85e8a4f7197a3580c8fabb
msrazavi <razavi.mst@gmail.com>
2022-08-30 04:26:15 +0430

    [ADD] join config

--- client/src/app/components/processor-configs/processor-config-components/join-config/join-config.component.ts
+++ client/src/app/components/processor-configs/processor-config-components/join-config/join-config.component.ts
@@ -7,6 +7,53 @@ import {Component, Input} from '@angular/core';
 })
 export class JoinConfigComponent {
     @Input() processorId: number = 0;
+    public selectedDataset: string = '';
+    public hasSelectedDataset: boolean = false;
+    public selectedLeft: string = '';
+    public selectedRight: string = '';
+    public selectedJoinType: string = '';
+    public joinTypes: string;
 
-    public constructor() {}
+    public constructor() {
+        this.joinTypes = 'Inner,Left,Right,Full';
+    }
+
+    public getDatasets(): string {
+        //TODO
+        //get all datasets from service from api
+        return 'dataset1,test2,my3';
+    }
+
+    public getSelectedDataset(e: string) {
+        this.selectedDataset = e;
+        this.hasSelectedDataset = true;
+        console.log(this.selectedDataset);
+    }
+
+    public getColumns(): string {
+        //TODO
+        //get columns of current source from service from api
+        return 'name,age,nationality,address,phone';
+    }
+
+    public getSelectedLeftVal(e: string) {
+        this.selectedLeft = e;
+        console.log(this.selectedLeft);
+    }
+
+    public getColumnsOfSelectedDataset(): string {
+        //TODO
+        //get columns of selected dataset from service from api
+        return 'color,shape,size';
+    }
+
+    public getSelectedRightVal(e: string) {
+        this.selectedRight = e;
+        console.log(this.selectedRight);
+    }
+
+    public getSelectedJoinType(e: string) {
+        this.selectedJoinType = e;
+        console.log(this.selectedJoinType);
+    }
 }
