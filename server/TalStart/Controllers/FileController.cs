a5574029bda5989deb2e4d1474a87140e1bc67f7
Arya Jalali <nothatcreative5@gmail.com>
2022-08-30 23:13:22 +0430

    Download

--- server/TalStart/Controllers/FileController.cs
+++ server/TalStart/Controllers/FileController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TalStart.IServices;
+using TalStart.IServices.IParserService;
 
 namespace TalStart.Controllers;
 
@@ -8,21 +9,30 @@ namespace TalStart.Controllers;
 public class FileController : ControllerBase
 {
     private readonly IFileService _fileService;
+    private readonly IParser _parser;
 
-    public FileController(IFileService fileService)
+    public FileController(IFileService fileService, IParser parser)
     {
         _fileService = fileService;
+        _parser = parser;
     }
 
-    [HttpPost("download")]
+    [HttpPost]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> DownloadFile([FromQuery] string datasetName,[FromQuery] string username)
+    public IActionResult DownloadFile([FromQuery] string datasetName,[FromQuery] string username)
     {
         try
         {
-            return new OkObjectResult(_fileService.DownloadFile(datasetName, username));
+            var tableName = $"{datasetName}_{username}";
+
+            var path = $"{AppContext.BaseDirectory}../../../resources/{username}/{datasetName}.csv";
+        
+            _parser.ParsePostgresTableToCsv(tableName, path);
+            var stream = new FileStream(path, FileMode.Open);
+            var res = new FileStreamResult(stream, "text/csv");
+            return File(res.FileStream, "text/csv", $"{datasetName}");
         }
         catch (Exception)
         {
